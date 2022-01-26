b82df0c1674a4db162d74575ef7489f158ffdfd1
Guilherme Policarpo <guilhermepc288@gmail.com>
2022-01-26 20:23:42 -0300

    Update conta.java

--- src/dio/banco/conta.java
+++ src/dio/banco/conta.java
@@ -1,6 +1,6 @@
 package dio.banco;
 
-public abstract class conta implements iConta{
+public abstract class Conta implements iConta{
 
     private static final int agencia_padrao = 1;
     private static int sequencial = 1;
@@ -8,16 +8,17 @@ public abstract class conta implements iConta{
     protected int agencia;
     protected int numero;
     protected double saldo;
-    protected cliente cliente;
+    protected Cliente cliente;
 
-    public conta(cliente cliente) {
-        this.agencia = conta.agencia_padrao;
+    public Conta(Cliente cliente) {
+        this.agencia = Conta.agencia_padrao;
         this.numero = sequencial++;
         this.cliente = cliente;
     }
 
     protected void imprimirExtratoConta(){
         System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+        System.out.println(String.format("CPF do Titular: %s", this.cliente.getCpf()));
         System.out.println(String.format("Agencia: %d", this.agencia));
         System.out.println(String.format("Numero: %d", this.numero));
         System.out.println(String.format("Saldo: %.2f", this.saldo));
@@ -34,7 +35,7 @@ public abstract class conta implements iConta{
     }
 
     @Override
-    public void transferir(double valor, conta contaDestino) {
+    public void transferir(double valor, Conta contaDestino) {
         this.sacar(valor);
         contaDestino.depositar(valor);
     }
